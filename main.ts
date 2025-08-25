50cdc0a9e8fb1b5e29725bdcf909b1ce737b5744
YaelRoeten <yaelroetenn@gmail.com>
2025-08-25 16:24:35 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 10) {
+        radio.sendNumber(20)
+    } else {
+        music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+    }
 })
+radio.setGroup(25)
+radio.setTransmitPower(7)
+radio.sendNumber(90)
+radio.setGroup(147)
+radio.setTransmitPower(1)
